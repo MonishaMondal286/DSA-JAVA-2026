44dea4e838954212b5e887a60d0273d9f40378ce
MonishaMondal286 <mondalmonisha286@gmail.com>
2026-04-03 16:02:23 +0530

    added

--- ARRAYS/intro.java
+++ ARRAYS/intro.java
@@ -1,26 +1,30 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
 package ARRAYS;
-import java.util.*;
+
+import java.util.Scanner;
 
 public class intro {
-    public static void main(String[] args) {
+   public intro() {
+   }
 
-        Scanner sc = new Scanner(System.in);
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      System.out.print("Enter the array size : ");
+      int var2 = var1.nextInt();
+      int[] var3 = new int[var2];
+      System.out.print("Enter the array elements : ");
 
-        System.out.print("Enter the array size : ");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
+      for(int var4 = 0; var4 < var2; ++var4) {
+         var3[var4] = var1.nextInt();
+      }
 
-        System.out.print("Enter the array elements : ");
-        for(int i=0;i<n;i++){
-            arr[i] = sc.nextInt();
-        }
+      System.out.print("Print negative arrays : ");
 
-        System.out.print("Print negative arrays : ");
-            for(int i=0;i<n;i++){
-                if(arr[i]<0){
-                    System.out.print(arr[i]+" ");
-                }
-            }
-        
-    }
+      for(int var5 = 0; var5 < var2; ++var5) {
+         if (var3[var5] < 0) {
+            System.out.print(var3[var5] + " ");
+         }
+      }
+
+   }
 }
